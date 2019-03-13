bd5abf922faf30a18c69f03fbaa32449c5428947
tangguanglong <2075394706@qq.com>
2019-03-13 13:35:46 +0800

    第一次作业

---
+++ 2.16/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int A;
+int B;
+int a,b,c,d;
+
+int main()
+{
+  cout<<"Enter two integers:";
+  cin>>A>>B;
+
+  a=A+B;
+  b=A-B;
+  c=A*B;
+  d=A/B;
+
+  cout<<"the product is"<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl;
+  cout<<d<<endl;
+}
---
+++ 2.20/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float r,d,l,s;
+    cin>>r;
+    d=2*3.14159;
+    l=2*3.14159*r;
+    s=3.14159*r*r;
+    cout<<"d="<<d<<endl;
+    cout<<"l="<<l<<endl;
+    cout<<"s="<<s<<endl;
+    return 0;
+}
